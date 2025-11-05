d6f25f09e828d3f76702938c6d214d3f1e9cd2ba
Thanapat1502 <psy150201027@gmail.com>
2025-11-05 16:14:54 +0700

    front-fix: mounted app body ui to home page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,13 +1,7 @@
-import Image from "next/image";
 import { AppTitle } from "@/components/share/AppTitle";
-import { UnauthenticatedManul } from "@/components/ui/home/UnauthenticatedManul";
-import { AuthenticatedApp } from "@/components/ui/home/AuthenticatedApp";
 import { AppBodyUi } from "@/components/ui/home/AppBodyUi";
 import { MainLayout } from "@/components/common/MainLayout";
 import { CustomFrame } from "@/components/common/CustomFrame";
-// import useAppStore from "@/store/zustand/useAppStore";
-// import useAuthStore from "@/store/zustand/useAuthStore";
-// import useUserStore from "@/store/zustand/useUserStore";
 
 export default function Home() {
   return (
@@ -15,7 +9,7 @@ export default function Home() {
       <CustomFrame className="flex flex-col gap-7">
         <AppTitle className="w-full" />
         {/* <UnauthenticatedManul /> */}
-        <AuthenticatedApp />
+        <AppBodyUi />
         {/* <AppBodyUi /> */}
       </CustomFrame>
     </MainLayout>
